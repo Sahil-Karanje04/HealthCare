43123b55b026c83cbea81f3ab281b5067d1b5e06
Sahil <sahilkaranje65@gmail.com>
2023-04-18 20:48:41 +0530

    added navbar

--- frontend/src/Pages/DoctorProfile/DoctorProfile.jsx
+++ frontend/src/Pages/DoctorProfile/DoctorProfile.jsx
@@ -3,7 +3,16 @@ import './DoctorProfile.css'
 
 const DoctorProfile = () => {
   return (
-    <div>DoctorProfile</div>
+    <div className='DoctorProfile-container'>
+        <div className="DF-left">
+            <div className="profile-image"></div>
+            <div className="profile-text">
+                <h3>Dr. Sharma</h3>
+                <h3 style={{color:"gray"}}>Qualifications...</h3>
+            </div>
+        </div>
+        <div className="DF-right"></div>
+    </div>
   )
 }
 
